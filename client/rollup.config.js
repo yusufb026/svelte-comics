35da8a7e9751881337ef55e5aa4bb2f012d7301a
yusufb026 <yusufbayim26@gmail.com>
2021-10-15 11:58:32 -0700

    Add terser config

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -87,7 +87,12 @@ export default {
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
-    production && terser(),
+    production &&
+      terser({
+        output: {
+          comments: false,
+        },
+      }),
   ],
   watch: {
     clearScreen: false,
